42bccae83f81c90fd272e34546e00c1d0667473e
rk1942 <rishabhkarthikeya@gmail.com>
2024-02-23 15:07:29 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import './App.css';
 import LikeImage from './components/LikeImage';
+import LikeImageHoc from './components/LikeImageHoc';
 import LikePost from './components/LikePost';
+import LikePostHoc from './components/LikePostHoc';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <div>
+
       <h3>Some Blog</h3>
+
+      {/* Render */}
+
       <div className='buttons'>
-        <LikePost/>
-        <LikeImage/>
+        <Wrapper render={(counter, handleIncreament)=>{
+          return <LikeImage likeImageCounter={counter} handleLikeImageCount = {handleIncreament}/>}}/>
+
+        <Wrapper render={(counter, handleIncreament)=>{
+          return <LikePost LikePostCounter={counter} handlePostCount = {handleIncreament}/>}}/>
       </div>
+
+
+{/* HOC */} 
+
+      <div className='buttons'>
+        <LikeImageHoc/>
+        <LikePostHoc/>
+      </div>
+
     </div>
   );
 }
